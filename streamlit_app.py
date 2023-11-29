80eb9c588e3d1e69d163cf9db35d35a6d7f1c92a
Regine0605 <152356882+Regine0605@users.noreply.github.com>
2023-11-29 13:15:54 +0800

    Update streamlit_app.py
    fix code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,20 @@
-import streamlit as st
-import pandas as pd
+import streamlit
+import pandas
 
-st.title('My Mom\'s New Healthy Diner')
+streamlit.title('My Moms New Healthy Diner')
 
-st.header('🥣 Breakfast Favorites')
-st.text('🥗 Omega 3 & Blueberry Oatmeal')
-st.text('🍞 Kale, Spinach & Rocket Smoothie')
-st.text('🐔 Hard-Boiled Free-Range Egg')
-st.text('🥑 Avocado Toast')
+streamlit.header('🥣 Breakfast Favorites')
+streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🍞 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑 Avocado Toast')
 
-st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-
-# Using st.sidebar to display the multiselect widget
-selected_fruits = st.sidebar.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberry'])
-
-# Do something with the selected fruits, if needed
-st.write("Selected Fruits:", selected_fruits)
-
-# Display the DataFrame in the main content area
-st.dataframe(my_fruit_list)
-
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberry'])
+streamlit.dataframe(my_fruit_list)
 
 
